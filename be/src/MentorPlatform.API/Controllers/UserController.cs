7526ceff8733e572c0ec03c1e21bb80d795682f0
Lam Tran <lamtran.duc.00@gmail.com>
2025-05-15 19:46:24 +0700

    fix as review
    - change query logic from repo to service
    - change some file name
    - add authorize

--- be/src/MentorPlatform.API/Controllers/UserController.cs
+++ be/src/MentorPlatform.API/Controllers/UserController.cs
@@ -1,15 +1,18 @@
 ﻿using MentorPlatform.Application.Commons.Models.Query;
 using MentorPlatform.Application.UseCases.UserManagement;
+using MentorPlatform.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MentorPlatform.WebApi.Controllers;
 
 [Route("api/users")]
+[Authorize(Roles = nameof(Role.Admin))]
 public class UserController : ApiControllerBase
 {
-    private readonly IUserService _userService;
+    private readonly IUserServices _userService;
 
-    public UserController(IUserService userService)
+    public UserController(IUserServices userService)
     {
         _userService = userService;
     }
@@ -21,17 +24,17 @@ public class UserController : ApiControllerBase
         return ProcessResult(result);
     }
 
-    [HttpPatch("activate/{userId}")]
+    [HttpPatch("{userId}/activate")]
     public async Task<IActionResult> ActivateUserAsync([FromRoute] Guid userId)
     {
-        var result = await _userService.ActivateUser(userId);
+        var result = await _userService.ChangeUserActive(userId);
         return ProcessResult(result);
     }
 
-    [HttpPatch("deactivate/{userId}")]
+    [HttpPatch("{userId}/deactivate")]
     public async Task<IActionResult> DeactivateUserAsync([FromRoute] Guid userId)
     {
-        var result = await _userService.DeactivateUser(userId);
+        var result = await _userService.ChangeUserActive(userId, false);
         return ProcessResult(result);
     }
 }
--- be/src/MentorPlatform.Application/UseCases/UserUseCases/UserServices.cs
+++ be/src/MentorPlatform.Application/UseCases/UserUseCases/UserServices.cs
@@ -2,23 +2,25 @@
 using MentorPlatform.Application.Commons.Mappings;
 using MentorPlatform.Application.Commons.Models.Query;
 using MentorPlatform.Application.Commons.Models.Responses.AuthResponses;
+using MentorPlatform.Domain.Entities;
+using MentorPlatform.Domain.Enums;
 using MentorPlatform.Domain.Repositories;
 using MentorPlatform.Domain.Shared;
 using System.Data;
 
 namespace MentorPlatform.Application.UseCases.UserManagement;
-public class UserService : IUserService
+public class UserServices : IUserServices
 {
     private readonly IUserRepository _userRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork)
+    public UserServices(IUserRepository userRepository, IUnitOfWork unitOfWork)
     {
         _userRepository = userRepository;
         _unitOfWork = unitOfWork;
     }
 
-    public async Task<Result> ActivateUser(Guid userId)
+    public async Task<Result> ChangeUserActive(Guid userId, bool isActive = true)
     {
         var dbUser = await _userRepository.GetByIdAsync(userId);
         if (dbUser == null)
@@ -26,21 +28,7 @@ public class UserService : IUserService
             return Result.Failure(UserErrors.UserNotExists);
         }
 
-        dbUser.IsActive = true;
-        await _unitOfWork.SaveChangesAsync();
-
-        return Result.Success();
-    }
-
-    public async Task<Result> DeactivateUser(Guid userId)
-    {
-        var dbUser = await _userRepository.GetByIdAsync(userId);
-        if (dbUser == null)
-        {
-            return Result.Failure(UserErrors.UserNotExists);
-        }
-
-        dbUser.IsActive = false;
+        dbUser.IsActive = isActive;
         await _unitOfWork.SaveChangesAsync();
 
         return Result.Success();
@@ -48,8 +36,14 @@ public class UserService : IUserService
 
     public async Task<Result<PaginationResult<UserResponse>>> Search(HasRoleQueryParameters query)
     {
-        var dbUsers = await _userRepository
-            .GetUsersByFullnameOrEmail(query.Search.Trim(), query.Role, (query.PageNumber - 1) * query.PageSize + 1, query.PageSize);
+        var keyword = query.Search.Trim().ToLower();
+        var dbQuery = _userRepository
+            .GetQueryable()
+            .Where(u => (query.Role == Role.All || u.Role == query.Role) && (u.UserDetail.FullName.ToLower().Contains(keyword) || u.Email.ToLower().Contains(keyword)))
+                .OrderBy(u => u.Id)
+                .Skip((query.PageNumber - 1) * query.PageSize)
+                .Take(query.PageSize);
+        var dbUsers = await _userRepository.ToListAsync(dbQuery, nameof(User.UserDetail));
         var userCount = dbUsers.Count;
         var pagination = new PaginationResult<UserResponse>(query.PageSize, query.PageNumber, userCount, dbUsers.Select(user => user.ToResponse()).ToList());
         return pagination;
