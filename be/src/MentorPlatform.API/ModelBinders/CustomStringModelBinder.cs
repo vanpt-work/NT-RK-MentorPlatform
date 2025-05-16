64f85cdd2aec57418a7c5f3b2663d0e9f4082256
LamTran93 <lamtdth2208062@fpt.edu.vn>
2025-05-16 16:16:23 +0700

    update

--- be/src/MentorPlatform.API/ModelBinders/CustomStringModelBinder.cs
+++ be/src/MentorPlatform.API/ModelBinders/CustomStringModelBinder.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 
 namespace MentorPlatform.WebApi.ModelBinders;
+
 public class CustomStringModelBinder : IModelBinder
 {
     public Task BindModelAsync(ModelBindingContext bindingContext)
@@ -20,40 +20,17 @@ public class CustomStringModelBinder : IModelBinder
         return Task.CompletedTask;
     }
 }
+
 public class TrimmingModelBinder : IModelBinder
 {
-    private readonly Dictionary<ModelMetadata, IModelBinder> _propertyBinders;
-    public TrimmingModelBinder(Dictionary<ModelMetadata, IModelBinder> propertyBinders)
-    {
-        _propertyBinders = propertyBinders;
-    }
+    private readonly IModelBinder _innerBinder;
+
+    public TrimmingModelBinder(IModelBinder innerBinder)
+        => _innerBinder = innerBinder;
 
     public async Task BindModelAsync(ModelBindingContext bindingContext)
     {
-        foreach (var entry in _propertyBinders)
-        {
-            var propertyMetadata = entry.Key;
-            var binder = entry.Value;
-
-            var modelName = ModelNames.CreatePropertyModelName(bindingContext.ModelName, propertyMetadata.PropertyName);
-
-            var propertyBindingContext = DefaultModelBindingContext.CreateBindingContext(
-                bindingContext.ActionContext,
-                bindingContext.ValueProvider,
-                propertyMetadata,
-                bindingInfo: null,
-                modelName
-            );
-
-            await binder.BindModelAsync(propertyBindingContext);
-
-            var resultModel = bindingContext.Result.Model;
-            if (propertyBindingContext.Result.IsModelSet)
-            {
-                propertyMetadata.PropertySetter(resultModel, propertyBindingContext.Result.Model);
-            }
-        }
-
+        await _innerBinder.BindModelAsync(bindingContext);
         if (!bindingContext.Result.IsModelSet || bindingContext.Result.Model == null)
             return;
 
@@ -95,30 +72,48 @@ public class TrimmingModelBinder : IModelBinder
 
 public class TrimmingModelBinderProvider : IModelBinderProvider
 {
-    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
     {
+        if (context == null)
+            throw new ArgumentNullException(nameof(context));
+
         if (context.Metadata.ModelType == typeof(string))
         {
             return new CustomStringModelBinder();
         }
 
-        if (context.Metadata.IsComplexType)
+        if (context.Metadata.IsComplexType || context.Metadata.IsCollectionType)
         {
             var propertyBinders = new Dictionary<ModelMetadata, IModelBinder>();
-
             foreach (var property in context.Metadata.Properties)
             {
                 var binder = context.CreateBinder(property);
-                if (binder != null)
-                {
-                    propertyBinders[property] = binder;
-                }
+                propertyBinders[property] = binder;
             }
 
-            return new TrimmingModelBinder(propertyBinders);
+            IModelBinder innerBinder;
+            if (context.Metadata.IsCollectionType)
+            {
+                var elementMetadata = context.Metadata.ElementMetadata;
+                if (elementMetadata == null)
+                {
+                    return null;
+                }
+
+                var elementBinder = context.CreateBinder(elementMetadata);
+                var loggerFactory = context.Services.GetRequiredService<ILoggerFactory>();
+
+                var collectionModelBinderType = typeof(CollectionModelBinder<>).MakeGenericType(elementMetadata.ModelType);
+                innerBinder = (IModelBinder)Activator.CreateInstance(collectionModelBinderType, elementBinder, loggerFactory);
+            }
+            else
+            {
+                innerBinder = new ComplexTypeModelBinder(propertyBinders, context.Services.GetRequiredService<ILoggerFactory>());
+            }
+
+            return new TrimmingModelBinder(innerBinder);
         }
 
         return null;
     }
 }
-
