92fdcae718426e2287813cf871d98f0c47e5a166
Lam Tran <lamtran.duc.00@gmail.com>
2025-05-20 00:12:04 +0700

    Update CourseServices.cs

--- be/src/MentorPlatform.Application/UseCases/CourseUseCases/CourseServices.cs
+++ be/src/MentorPlatform.Application/UseCases/CourseUseCases/CourseServices.cs
@@ -1,4 +1,14 @@
-﻿namespace MentorPlatform.Application.UseCases.CourseUseCases;
-public class CourseServices
+﻿using MentorPlatform.Domain.Repositories;
+
+namespace MentorPlatform.Application.UseCases.CourseUseCases;
+public class CourseServices : ICourseServices
 {
+    private readonly ICourseRepository _courseRepository;
+
+    public CourseServices(ICourseRepository courseRepository)
+    {
+        _courseRepository = courseRepository;
+    }
+
+
 }
