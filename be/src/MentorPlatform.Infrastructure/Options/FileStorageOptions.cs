b4d53e6f1ec61ac41f40c49f8ae9f56ebdc17c18
pine04 <tunggnut2004@gmail.com>
2025-05-24 22:32:10 +0700

    Move media storage options out of CloudinaryOptions and into the parent FileStorageOptions.

--- be/src/MentorPlatform.Infrastructure/Options/FileStorageOptions.cs
+++ be/src/MentorPlatform.Infrastructure/Options/FileStorageOptions.cs
@@ -3,8 +3,37 @@ namespace MentorPlatform.Infrastructure.Options;
 
 public class FileStorageOptions
 {
+    public string FileStorageProvider { get; set; } = default!;
+    public MediaSettings MediaSettings { get; set; } = default!;
     public AWSS3StorageOptions? AWSS3StorageOptions { get; set; } = default;
     public CloudinaryStorageOptions? CloudinaryStorageOptions { get; set; } = default;
 
-    public string FileStorageProvider { get; set; } = default!;
+}
+
+public class MediaSettings
+{
+    public ImageSettings Images { get; set; } = default!;
+    public VideoSettings Videos { get; set; } = default!;
+    public DocumentSettings Documents { get; set; } = default!;
+}
+
+public class ImageSettings
+{
+    public string[] AllowedExtensions { get; set; } = default!;
+    public long MaxSizeBytes { get; set; } = default!;
+    public string FolderPath { get; set; } = default!;
+}
+
+public class VideoSettings
+{
+    public string[] AllowedExtensions { get; set; } = default!;
+    public long MaxSizeBytes { get; set; } = default!;
+    public string FolderPath { get; set; } = default!;
+}
+
+public class DocumentSettings
+{
+    public string[] AllowedExtensions { get; set; } = default!;
+    public long MaxSizeBytes { get; set; } = default!;
+    public string FolderPath { get; set; } = default!;
 }
